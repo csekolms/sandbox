2847f7af779a662b7a64590815ebec8403e54475
Csekő László <laszlo.cseko@meta-inf.hu>
2024-11-28 11:01:53 +0100

    F-1 added common violations

--- src/main/java/hu/csekolms/sandbox/Service.java
+++ src/main/java/hu/csekolms/sandbox/Service.java
@@ -1,7 +1,11 @@
 package hu.csekolms.sandbox;
 
 public class Service {
+    private static final int UNUSED_CONSTANT = -1;
     public void something() {
         int unused = 0;
+    }
+    public void tested(){
+        System.out.println("tested()");
     }
 }
---
+++ src/test/java/hu/csekolms/sandbox/ServiceTest.java
@@ -0,0 +1,13 @@
+package hu.csekolms.sandbox;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+
+    @Test
+    void tested() {
+        new Service().tested();
+    }
+}
