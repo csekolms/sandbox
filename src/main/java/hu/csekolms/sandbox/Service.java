62c3a2c6eecd2d0dce9d4dd86141f044ae0a4fe2
Csekő László <laszlo.cseko@meta-inf.hu>
2024-11-28 11:26:45 +0100

    F-1 Trying to trigger codeql violation

--- src/main/java/hu/csekolms/sandbox/Service.java
+++ src/main/java/hu/csekolms/sandbox/Service.java
@@ -1,5 +1,14 @@
 package hu.csekolms.sandbox;
 
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
 public class Service {
     private static final int UNUSED_CONSTANT = -1;
     public void something() {
@@ -7,5 +16,13 @@ public class Service {
     }
     public void tested(){
         System.out.println("tested()");
+    }
+    // Trying to trigger codeql violation https://codeql.github.com/codeql-query-help/java/java-static-initialization-vector/
+    void staticIV() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
+        byte[] iv = new byte[16]; // all zeroes
+        GCMParameterSpec params = new GCMParameterSpec(128, iv);
+        Cipher cipher = Cipher.getInstance("AES/GCM/PKCS5PADDING");
+        Key key = new SecretKeySpec(iv, "AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key, params);
     }
 }
